23d57b7193bd67ea19f49761a4fd3cc641a60ee3
watanaberaphael <watanabe.raphael@gmail.com>
2016-06-19 14:22:56 -0300

    Test and Package

---
+++ src/github.com/watanaberaphael/hello/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/watanaberaphael/stringutil"
+	)
+
+func main() {
+	
+	fmt.Printf("hello, world\n")
+
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+
+	fmt.Printf("\n")
+	
+}
